3550c447b2685275aa18073af0e36ff4277f3eea
Nixes <callum@see-bry.com>
2016-09-15 23:04:52 +1000

    more travis fiddling

--- ci/install.sh
+++ ci/install.sh
@@ -8,7 +8,7 @@ install_c_toolchain() {
     case $TARGET in
         aarch64-unknown-linux-gnu)
             sudo apt-get install -y --no-install-recommends \
-                 gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross libfreetype6-dev
+                 gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
             ;;
         *)
             # For other targets, this is handled by addons.apt.packages in .travis.yml
